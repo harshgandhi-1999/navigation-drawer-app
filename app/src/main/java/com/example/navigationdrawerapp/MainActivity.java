26d0774ca27e9c6536c4f257dd6141c00329c3b4
harsh gandhi <harshgandhi043@gmail.com>
2022-02-20 00:53:50 +0530

    Added some new files  - Added navigation header file  - Added new font family  - Made top App bar using material ui

--- app/src/main/java/com/example/navigationdrawerapp/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawerapp/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.navigationdrawerapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+
     }
 }
