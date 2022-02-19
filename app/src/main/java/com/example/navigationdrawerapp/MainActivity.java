03a791bfc8ccb88fe2fa595301de3262bf6dd53f
harsh gandhi <harshgandhi043@gmail.com>
2022-02-20 01:20:10 +0530

    Implemented opening and closing of drawer on button click

--- app/src/main/java/com/example/navigationdrawerapp/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawerapp/MainActivity.java
@@ -1,25 +1,74 @@
 package com.example.navigationdrawerapp;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle actionBarDrawerToggle;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout = findViewById(R.id.drawerLayout);
 
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        MaterialToolbar topAppBar = findViewById(R.id.topAppBar);
+        NavigationView navigationView = findViewById(R.id.navigationView);
+
+        topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                String message = "";
+                switch (id){
+                    case R.id.homeMenuItem:
+                        message = "Home is clicked";
+                        break;
+                    case R.id.messagesMenuItem:
+                        message = "Messages is clicked";
+                        break;
+                    case R.id.syncMenuItem:
+                        message = "Sync is clicked";
+                        break;
+                    case R.id.trashMenuItem:
+                        message = "Trash is clicked";
+                        break;
+                    case R.id.settingsMenuItem:
+                        message = "Settings is clicked";
+                        break;
+                    case R.id.loginMenuItem:
+                        message = "Login is clicked";
+                        break;
+                    case R.id.shareMenuItem:
+                        message = "Share is clicked";
+                        break;
+                    case R.id.rateMenuItem:
+                        message = "Rate Us is clicked";
+                        break;
+                }
+
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 }
